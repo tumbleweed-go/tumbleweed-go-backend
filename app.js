89c1d7aca64010118ca683798b334cd97245ddcd
Max Huang <letmaxknow@gmail.com>
2020-07-18 18:46:09 -0400

    Updated upload with image recognition

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 let tumbleweedUploadRouter = require('./routes/tumbleweed/upload');
 let tumbleweedGetRouter = require('./routes/tumbleweed/get');
+//let tumbleweedDetectRouter = require('./routes/tumbleweed/detect');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/tumbleweed/upload', tumbleweedUploadRouter);
 app.use('/tumbleweed/get', tumbleweedGetRouter);
+//app.use('/tumbleweed/detect', tumbleweedDetectRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/tumbleweed/upload.js
+++ routes/tumbleweed/upload.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const vision = require('@google-cloud/vision');
+const colorsys = require('colorsys');
 const firebase = require('firebase');
 
 const fb = require('./../../utils/firebase');
+const upload = multer({ dest: './uploads' });
+
+// File upload fields for POST request.
+const uploadFields = [
+  { name: 'image', maxCount: 1 }
+];
+
+const isTumbleweedColour = (hsv) => {
+
+  let range = {
+    h: { low: 25, high: 45 },
+    s: { low: 45, high: 65 },
+    v: { low: 50, high: 80 }
+  };
+
+  // Check for correct hue.
+  if (hsv.h < range.h.low || hsv.h > range.h.high) {
+    return false;
+  }
+  // Check for correct saturation.
+  if (hsv.s < range.s.low || hsv.s > range.s.high) {
+    return false;
+  }
+  // Check for correct value.
+  if (hsv.v < range.v.low || hsv.v > range.v.high) {
+    return false;
+  }
+
+  return true;
+}
 
 // Enable CORS.
 router.use((req, res, next) => {
@@ -11,28 +44,69 @@ router.use((req, res, next) => {
   next();
 });
 
-router.post('/:latitude/:longitude', async (req, res, next) => {
+router.post('/:latitude/:longitude', upload.fields(uploadFields), async (req, res, next) => {
+
+  // INPUT
+  
+  // Validate file input.
+  if (!req.files) {
+    return res.status(500).json({ result: 'Missing req.files' });
+  }
+  if (!req.files.image) {
+    return res.status(400).json({ result: 'Missing image' });
+  }
 
   // Parse input from req.params.
   let latitude = parseFloat(req.params.latitude);
   let longitude = parseFloat(req.params.longitude);
 
-  // Check if coordinates are numbers.
+  // Validate coordinate input.
   if (isNaN(latitude) || isNaN(longitude)) {
     return res.status(400).json({ result: 'Invalid latitude or longitude.' });
   }
-  // Check for valid latitude.
   if (latitude < -90 || latitude > 90) {
     return res.status(400).json({ result: 'Latitude out of range.' });
   }
-  // Check for valid longitude.
   if (longitude < -180 || longitude > 180) {
     return res.status(400).json({ result: 'Longitude out of range.' });
   }
 
-  // Round to 5 decimal places.
+  // Round coordinates to 5 decimal places.
   latitude = Math.round(latitude * 100000) / 100000;
   longitude = Math.round(longitude * 100000) / 100000;
+
+  // IMAGE DETECTION
+
+  let isPlant = false;
+  let correctColour = false
+
+  let imageFileName = req.files.image[0].filename;
+  let imageDir = __dirname + `/../../uploads/${imageFileName}`;
+
+  // Detect if image has plant.
+  let [ labelPropertiesResults ] = await new vision.ImageAnnotatorClient().labelDetection(imageDir);
+  let labels = labelPropertiesResults.labelAnnotations;
+  labels.forEach(label => {
+    if (label.description === 'Plant') {
+      isPlant = true;
+    }
+  });
+
+  // Detect if image is the correct colour.
+  let [ imagePropertiesResult ] = await new vision.ImageAnnotatorClient().imageProperties(imageDir);
+  let colours = imagePropertiesResult.imagePropertiesAnnotation.dominantColors.colors;
+  if (colours.length > 0) {  // Only check the first colour (the most prominent one).
+    let rgb = colours[0].color;
+    let hsv = colorsys.rgb_to_hsv(rgb.red, rgb.green, rgb.blue);
+    correctColour = isTumbleweedColour(hsv);
+  }
+
+  // Exit if not tumbleweed.
+  if (!isPlant || !correctColour) {
+    return res.status(400).json({ result: 'Image doesn\'t contain a tumbleweed.' });
+  }
+
+  // UPLOAD COORDINATES
 
   let promise = new Promise(resolve => {
     // Add tumbleweed to firestore.
